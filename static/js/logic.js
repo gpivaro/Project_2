2b0508e056444d9a0acc59a43b9ccfa72980cc5a
Gabriel Pivaro <gabrconc@gmail.com>
2020-12-08 21:34:50 -0600

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -18,10 +18,10 @@ var myMap = L.map("map", {
   var aircraftIcon = L.icon({
     iconUrl: 'Resources/Airplane_wwwroot_uploads_svg_symbol_0qvhey5-airplane-vector.svg',
     
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
+    iconSize:     [38/3, 95/3], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
+//     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
   
