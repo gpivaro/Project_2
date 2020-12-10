70354db0b59021bfcb7c6abcff85863c3ccfa5e4
Gabriel Pivaro <gabrconc@gmail.com>
2020-12-09 19:33:17 -0600

    dropdown menu

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 import json
 
 # # Import database user and password
-# from api_keys import mysql_hostname
-# from api_keys import mysql_port
-# from api_keys import mysql_user_project2
-# from api_keys import mysql_pass_project2
+from api_keys import mysql_hostname
+from api_keys import mysql_port
+from api_keys import mysql_user_project2
+from api_keys import mysql_pass_project2
 
 # mysql_hostname = os.environ['MYSQL_HOSTNAME']
 # print(mysql_hostname)
