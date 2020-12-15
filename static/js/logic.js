b38e1191e69feebc054aef58811026f1a7643b0b
Gabriel Pivaro <gabrconc@gmail.com>
2020-12-15 17:10:52 -0600

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -477,8 +477,7 @@ d3.json(url).then(function (data) {
     // text: flightData.map(element => element.callsign),
     text: flightData,
     hovertemplate: "<b>Aircraft Info:</b><br><br>" + "ICAO24: %{text.icao24}<br>" + "Callsign: %{text.callsign}<br>" +
-      "Vertical rate: %{text.vertical_rate} m/s<br>" + "Altitude %{text.baro_altitude} m"+ 
-    "Click on the dot for more info.",
+      "Vertical rate: %{text.vertical_rate} m/s<br>" + "Altitude %{text.baro_altitude} m<br>"+ "Click on the dot for more info.",
     mode: 'markers',
     type: 'scatter'
   };
